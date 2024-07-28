f7b65d8e4dbbd332e67571a1a51707b20c2b6fe8
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-28 23:42:54 +0100

    change table case type

--- task_6.sql
+++ task_6.sql
@@ -2,8 +2,9 @@
 USE alx_book_store;
 
 -- Insert multiple rows into the Customers table
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+"INSERT INTO customer", "124 Happiness  Ave."
